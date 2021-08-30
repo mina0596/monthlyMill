99bd3f53abb66898ccbb7f0563ffc89016228bb7
Minah Park <mina0596@hotmail.com>
2021-08-30 13:35:05 +0900

    [민아]
    id중복확인 처리 작업중

--- monthlyMill/src/main/java/project/monthlyMill/controller/JoinController.java
+++ monthlyMill/src/main/java/project/monthlyMill/controller/JoinController.java
@@ -1,5 +1,8 @@
 package project.monthlyMill.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import project.monthlyMill.service.JoinService;
+
 @Controller
 @RequestMapping("/join")
 public class JoinController {
+	
+	private static final Logger log = LoggerFactory.getLogger(JoinController.class);
+	
+	private final JoinService joinService;
+	
+	@Autowired
+	public JoinController(JoinService joinService) {
+		this.joinService = joinService;
+	}
 	
 	//회원가입
 	@GetMapping("/join_basic")
@@ -19,9 +33,11 @@ public class JoinController {
 	
 	@PostMapping("/memberIdCheck")
 	@ResponseBody
-	public Boolean getMemberIdCheckResult(@RequestParam (value = "inputId") String inputId) {
-		
-		return true;
+	public boolean getMemberIdCheckResult(@RequestParam (name = "inputId", required = false) String inputId) {
+		log.info("입력된 아이디 확인: {}", inputId);
+		boolean idCheckResult = joinService.getMemberInfoById(inputId);
+		log.info("아이디 중복 확인: {}", idCheckResult);
+		return idCheckResult;
 	}
 
 }
--- monthlyMill/src/main/resources/static/js/memberJoin.js
+++ monthlyMill/src/main/resources/static/js/memberJoin.js
@@ -2,26 +2,26 @@
  * 
  */
 $(function(){
-	$('#idCheck').click(function(){
+	$('.idCheck').click(function(){
 		var inputId = $('input[name="inputId"]').val();
 				console.log('inputId: ' + inputId);
 				if(inputId != '' && inputId != undefined && inputId != null){
 					var request = $.ajax({
 						url: "/join/memberIdCheck",
 						method: "POST",
-						data: { employeeId : employeeId }, 
+						data: { inputId : inputId }, 
 						dataType: "json"
 					});
 					request.done(function( data ) {
 						if(data){
 							alert("사용가능한 아이디입니다.");
 							
-							$('#addEmployeeForm input').prop('disabled', false);
-							$('#addEmployeeForm select').prop('disabled', false);
-							$('#addEmployeeForm button').prop('disabled', false);
+							$('#memberJoinForm input').prop('disabled', false);
+							$('#memberJoinForm select').prop('disabled', false);
+							$('#memberJoinForm button').prop('disabled', false);
 						}else{
 							alert('중복된 아이디입니다.');
-							$('input[name="employeeId"]').focus();
+							$('input[name="inputId"]').focus();
 						}
 						
 					}); 
