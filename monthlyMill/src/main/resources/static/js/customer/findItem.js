8069c347a26449e6505decbf967520eb8bbdfa2d
hy <mintlib66@gmail.com>
2021-10-01 18:31:27 +0900

    [하영]findItem오류 수정 - 이미지 파일 리사이즈 - main수정중

--- monthlyMill/src/main/resources/static/js/customer/findItem.js
+++ monthlyMill/src/main/resources/static/js/customer/findItem.js
@@ -9,20 +9,29 @@ $(function(){
 	var resultHtml = '';
 	
 	$(document).on('click', '#resultBtn',  function(){
-		document.getElementsByClassName('result_hash_list').remove;
-		document.getElementsByClassName('hash').remove;
-		$('.hash').remove();
+		document.querySelector('.result_hash_list').innerHTML='';
+		//document.getElementsByClassName('hash').remove;
+		//$('.hash').remove();
 		$('.result_beforeText').remove();
 		$('.result_item').remove();
 		html='';
+		selectedTagsName = [];
+		selectedTags = [];
+		selectedMidClass = [];
 		
 		
 		// ********************************** 선택한 해시태그 화면단에 뿌려주기************************
 		$('.hashCheck').each(function(){
+			
 			if($(this).is(':checked')){
+				
 				selectedTagsName.push($(this).attr('id'));
 				selectedTags.push($(this).val());
 				selectedMidClass.push($(this).parent().parent().children().find('.hash_box__title').text());
+				console.log("===============")
+				console.log(selectedTagsName); 
+				console.log(selectedTags); 
+				console.log(selectedMidClass); 
 			}
 		});
 		
@@ -42,45 +51,63 @@ $(function(){
 		});
 		
 		request.done(function( rcmdResult ){
+			$('.result_item').remove();
+			html = '';
 			if(selectedTagsName.length != 0){
-				html = '<ol class="result_hash_list">';
+				//selectedTagName에 기존내용이 계속 중첩되는 오류있음.
 				for(var i=0; i<selectedTagsName.length; i++){
 					html += '<li class="hash">' + selectedTagsName[i] + '</li>';
 				}
+				$('.result_hash_list').html(html);
+				
 			}else{
-				html += '<li class="hash">태그를 선택해주세요</li>';
+				html = `<span>태그를 선택해주세요.</span>`;
+				$('.text_box').html(html);
 			}
-			$('.result_hash').append(html);
+
 			
-			$('.result_item').remove();
 			if(rcmdResult.length != 0){
-				resultHtml = '<tr class="result_item">';
+				resultHtml = '';
 				for(var j=0; j<rcmdResult.length; j++){
-					resultHtml += '<td><span class="result_item__num">'+ j + '</span></td>';
-					resultHtml += '<td><img src="" alt="제품이미지" class="result_item__img"></td>';
-					resultHtml += '<td><div class="td_column"><span class="result_item__title">' + rcmdResult[j].pName + '</span>';
-					resultHtml += '<input name="pCode" class="result_item_code" type="hidden" value="' + rcmdResult[j].pCode + '">';
-					resultHtml += '<button class="result_item__nutrient">영양정보</button></div></td>';
-					resultHtml += '<td><span class="result_item__price">' + rcmdResult[j].pPrice + '원' + '</span></td>';
-					resultHtml += '<td><input type="submit" class="result_item__btnCart" value="장바구니"></td>';
-					resultHtml += '</tr></form>';
+					resultHtml += `
+					<tr class="result_item">
+						<td>
+							<span class="result_item__num">${j}</span>
+						</td>
+						<td>
+							<img src="" alt="제품이미지" class="result_item__img">
+						</td>
+						<td>
+							<div class="td_column">
+								<span class="result_item__title">${rcmdResult[j].pName}</span>
+								<input name="pCode" class="result_item_code" type="hidden" value="${rcmdResult[j].pCode}">
+								<button class="result_item__nutrient readDetail">
+									<i class="fas fa-search-plus"></i>
+									<span>영양정보</span>
+								</button>
+							</div>
+						</td>
+						<td>
+							<span class="result_item__price price_number">${rcmdResult[j].pPrice}</span>
+						</td>
+						<td>
+							<input type="submit" class="button result_item__btnCart" value="장바구니" />
+						</td>
+					</tr>`;
 				}
+				$('.result_table_body').html(resultHtml);
 			}else{
-				resultHtml += '<tr class="result_item">';
-				resultHtml += '<td><span class="no_result">선택하신 해시태그에 해당하는 상품이 없습니다.</span></td>';
-			}
-			$('.result_table_body').append(resultHtml);
+				$('.text_box').html('<span>선택하신 해시태그에 해당하는 상품이 없습니다.</span>');
+			}	
 		});
 		
 		request.fail(function( jqXHR, textStatus ) {
 	 		$('.result_item').remove();
-			$('.result_table_body').append('<tr>선택하신 해시태그에 해당하는 상품이 없습니다.</tr>');
+			$('.text_box').html('<span>선택하신 해시태그에 해당하는 상품이 없습니다.</span>');
 		});
-		
 		
 		//*********************************** 장바구니******************************
 		$(document).on('click', '.result_item__btnCart', function(){
-			
 			var param = $(this).parent().parent().children().find('.result_item_code').val();
 			var pName = $(this).parent().parent().children().find('.result_item__title').text();
 			var params = { 'pCode' : param };
@@ -105,6 +132,5 @@ $(function(){
 			
 		});
 	})
-	
-	
+
 });
