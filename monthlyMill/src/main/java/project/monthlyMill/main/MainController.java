032d7acc5e8cb8da9516e4b9385aa9cd10107fe4
Minah Park <mina0596@hotmail.com>
2021-10-08 00:36:14 +0900

    [민아]
    main에 로그인정보 넣기

--- monthlyMill/src/main/java/project/monthlyMill/main/MainController.java
+++ monthlyMill/src/main/java/project/monthlyMill/main/MainController.java
@@ -1,12 +1,18 @@
 package project.monthlyMill.main;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 	@GetMapping("/")
-	public String main() {
+	public String main(HttpSession session, Model model) {
+		if(session.getAttribute("SMNUM") != null) {
+			model.addAttribute("loginCheck", session.getAttribute("SMNUM"));
+		}
 		return "/common/main";
 	}
 }
