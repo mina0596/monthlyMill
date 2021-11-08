bfe99ae4bcce33c15a0e7bf301efe593a73a1bec
Minah Park <mina0596@hotmail.com>
2021-11-08 11:58:04 +0900

    [민아]
    입점정보 쿼리

--- monthlyMill/src/main/java/project/monthlyMill/makers/myPage/MakersMyPageMapper.java
+++ monthlyMill/src/main/java/project/monthlyMill/makers/myPage/MakersMyPageMapper.java
@@ -2,6 +2,7 @@ package project.monthlyMill.makers.myPage;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import project.monthlyMill.dto.MakerStore;
 import project.monthlyMill.dto.Member;
 
 @Mapper
@@ -9,4 +10,7 @@ public interface MakersMyPageMapper {
 	
 	// 메이커스의 회원 기본정보 아이디로 가져오기
 	public Member getMakersInfoById(String makerId);
+	
+	// 메이커스의 입점정보 아이디로 가져오기 
+	public MakerStore getStoreInfoById(String makerId);
 }
--- monthlyMill/src/main/resources/static/js/makers/makerMypage.js
+++ monthlyMill/src/main/resources/static/js/makers/makerMypage.js
@@ -3,13 +3,10 @@ $(document).on("click", ".mypage-infoBox-enterBtn", function(){
     //enter화면으로 넘어가도록 
 });
 
-/*====== 패스워드 재입력창 ======*/
-$(document).on("click", ".mypage-infoBox-pwConfirmBtn", function(){
-    //pwCheck에 패스워드 일치여부 T/F값 넣어주세요
+
+/* ====== 패스워드 확인하는 함수 ======*/
+function pWcheck(inputPw, inputUrl){
     let pwCheck = false;
-	
-	var inputPw = $('.mypage-infoBox-input').val();
-	
 	$.ajax({
 		url: "/makers/myPage/pwConfirm",
 		method: "post",
@@ -18,13 +15,23 @@ $(document).on("click", ".mypage-infoBox-pwConfirmBtn", function(){
 		success: function(result){
 			if(result.pwCheck){
 		        //패스워드 일치 시 다음화면으로 넘어가게
-				location.href="/makers/myPage/user/editBasicInfo";
+				pwCheck = true;
+				location.href=inputUrl;
 			}else{
 			    //패스워드 불일치 시
 		        $(".mypage-infoBox-pwValidatingMsg").text("비밀번호가 일치하지 않습니다.");
 			}
 		}
 	});
+}
+
+/*====== 패스워드 재입력창 ======*/
+$(document).on("click", ".mypage-infoBox-pwConfirmBtn", function(){
+    //pwCheck에 패스워드 일치여부 T/F값 넣어주세요
+	
+	var inputPw = $('.mypage-infoBox-input').val();
+	var inputUrl = "/makers/myPage/user/editBasicInfo";
+	pWcheck(inputPw, inputUrl);
 	
 });
 
@@ -37,9 +44,21 @@ $(document).on("click", ".mypage-infoBox-pwCancelBtn", function(){
 
 /* ============================[민아]====================================*/
 
-/*====== 메이커스 기본정보 읽기전용화면에서 수정화면으로 이동 ======*/
+/*====== 메이커스 기본정보 읽기전용화면에서 비번확인 화면으로 이동 ======*/
 $(document).on("click", ".info-updateBtn", function(){
-    //이전 페이지로
+    
     location.href="/makers/myPage/user/confirmPw";
 });
 
+/*====== 메이커스 입점정보 읽기전용화면에서 비번확인 화면으로 이동 ======*/
+$(document).on("click", ".info-updateBtn", function(){
+   
+    location.href="/makers/myPage/store/confirmPw";
+});
+
+$(function(){
+	var enterCheck = $('.enterCheck').val();
+	if(enterCheck == 'Y'){
+		// 입정승인이 승인일 경우
+	}
+})
